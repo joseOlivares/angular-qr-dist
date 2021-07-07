7c6ea8f9552bf88c672cb6e3e32fb2b61429fe35
José Luis <joseluis_503@yahoo.com>
2021-07-06 19:03:55 -0600

    node server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express= require('express');
 var app = express();
 var server=require('http').createServer(app);
-var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/dist')); //serving statics files like css, js, images
 var port=process.env.PORT || 3000; //this is for heroku
